4d11d113c16b81cfa75bd734a94a741659c4aae5
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2024-12-08 21:55:40 +0530

    demo_card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SkillsComponent } from 'src/skills/skills.component';
 import { StudentComponent } from './student/student.component';
+import { CardComponent } from './card/card.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,SkillsComponent,StudentComponent
+    AppComponent,SkillsComponent,StudentComponent, CardComponent, 
   ],
   imports: [
     BrowserModule
